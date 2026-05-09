b1d82427bf933c652d1b7e8a4f28d713f8c652e1
Saima Khan <saimakhancsedu@gmail.com>
2026-05-09 08:53:30 +0600

    edited

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,37 +1,3 @@
-// import Agencies from "../components/Agencies";
-// import ChasingConsumers from "../components/ChasingConsumers";
-// import FeaturedWork from "../components/FeaturedWork";
-// import Footer from "../components/Footer";
-// import GlobalTeam from "../components/GlobalTeam";
-// import Hero from "../components/Hero";
-// import LegacySection from "../components/LegacySection";
-// import Navbar from "../components/Navbar";
-// import ReadyToRise from "../components/ReadyToRise";
-// import Services from "../components/Services";
-// import TopBar from "../components/TopBar";
-// import WhatsNew from "../components/WhatsNew";
-
-// function MainLayout() {
-//   return (
-//     <div className="">
-//       <TopBar />
-//       <Navbar />
-//       <Hero />
-//       <Agencies />
-//       <GlobalTeam></GlobalTeam>
-//       <FeaturedWork></FeaturedWork>
-//       <Services></Services>
-//       <ChasingConsumers></ChasingConsumers>
-//       <LegacySection></LegacySection>
-//       <WhatsNew></WhatsNew>
-//       <ReadyToRise></ReadyToRise>
-//       <Footer> </Footer>
-//     </div>
-//   );
-// }
-
-// export default MainLayout;
-
 import React from "react";
 import { motion } from "framer-motion";
 
